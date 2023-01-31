6dbd589d29072f216f3b4f79adcf575e5bb42a6a
Madison Evans <m.corbinevans@gmail.com>
2023-01-31 16:26:43 -0700

    canvas added

--- src/components/ContentSection/ContentSection.jsx
+++ src/components/ContentSection/ContentSection.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./ContentSection.css";
 import Palette from "../Palette/Palette";
 
 const ContentSection = () => {
 	return (
 		<>
 			<div className="content-section-container">
-				<div className="canvas">canvas goes here</div>
+				<div className="canvas">
+					<canvas></canvas>
+				</div>
 				<h2>Palette</h2>
 				<Palette></Palette>
 			</div>
