bfc1ac34f66b17f7bc4283d859f3144a3244a7af
Madison Evans <m.corbinevans@gmail.com>
2023-01-31 15:36:21 -0700

    app started. contentsection created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ContentSection from "./components/ContentSection/ContentSection";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<ContentSection></ContentSection>
+		</div>
+	);
 }
 
 export default App;
