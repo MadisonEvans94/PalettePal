08e64a75815cb512429b9882f825d525c4aa1f5a
Madison Evans <m.corbinevans@gmail.com>
2023-02-01 14:53:25 -0700

    checkpoint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Input from "./components/Input/Input";
 import ContentSection from "./components/ContentSection/ContentSection";
 import SideBar from "./components/SideBar/SideBar";
+import { CanvasContext } from "./Contexts/CanvasContext";
 function App() {
 	return (
 		<div className="App">
-			<SideBar>
-				<Input />
-			</SideBar>
-			<ContentSection></ContentSection>
+			<CanvasContext.Provider values={{}}>
+				<SideBar>
+					<Input />
+				</SideBar>
+				<ContentSection></ContentSection>
+			</CanvasContext.Provider>
 		</div>
 	);
 }
