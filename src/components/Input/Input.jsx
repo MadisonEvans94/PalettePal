ea19c1ceda27659f2bb21f0529ee4787359eb33b
Madison Evans <m.corbinevans@gmail.com>
2023-02-01 13:15:45 -0700

    input component renders image to screen

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Input.css";
+import { useState, useEffect, useRef } from "react";
+
+//[ ] find a react equivalent to change event listener on input
+
 const Input = () => {
+	const [imageUrl, setImageUrl] = useState(null);
+
+	const handleFileChange = (e) => {
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			setImageUrl(e.target.result);
+			console.log(e.target.result);
+		};
+		reader.readAsDataURL(e.target.files[0]);
+	};
+
 	return (
-		<>
-			<div className="input-container">
-				<input type="file" id="image-input" accept="image/png, image/jpg" />
-			</div>
-		</>
+		<div>
+			<input type="file" accept="image/jpeg" onChange={handleFileChange} />
+			{imageUrl && <img src={imageUrl} alt="Uploade" />}
+		</div>
 	);
 };
 
