fe2dce2c835aae2cbf1a65cb618fc7e6d209775e
Madison Evans <m.corbinevans@gmail.com>
2023-02-02 11:20:48 -0700

    kmeans works now

--- src/prototyping/CustomPlot.jsx
+++ src/prototyping/CustomPlot.jsx
@@ -2,6 +2,117 @@ import React from "react";
 import Plot from "react-plotly.js";
 
 /**
+ * Compute the Euclidean distance between two points
+ * @param {Array} point1 - The first point in the form of [R, G, B]
+ * @param {Array} point2 - The second point in the form of [R, G, B]
+ * @returns {number} The Euclidean distance between the two points
+ */
+const euclideanDistance = (point1, point2) => {
+	let distance = 0;
+	for (let i = 0; i < point1.length; i++) {
+		distance += (point1[i] - point2[i]) ** 2;
+	}
+	return Math.sqrt(distance);
+};
+
+/**
+ * Find the closest centroid to a given point
+ * @param {Array} point - The point in the form of [R, G, B]
+ * @param {Array} centroids - An array of centroids in the form of [[R, G, B], [R, G, B], ...]
+ * @returns {number} The index of the closest centroid in the centroids array
+ */
+const findClosestCentroid = (point, centroids) => {
+	let minDistance = Infinity;
+	let closestCentroidIndex = 0;
+	for (let i = 0; i < centroids.length; i++) {
+		let distance = euclideanDistance(point, centroids[i]);
+		if (distance < minDistance) {
+			minDistance = distance;
+			closestCentroidIndex = i;
+		}
+	}
+	return closestCentroidIndex;
+};
+
+/**
+ * Compute the new centroids based on the closest centroids of each point
+ * @param {Array} points - An array of points in the form of [[R, G, B], [R, G, B], ...]
+ * @param {Array} closestCentroids - An array of the indices of the closest centroids for each point
+ * @param {number} K - The number of centroids
+ * @returns {Array} An array of the new centroids in the form of [[R, G, B], [R, G, B], ...]
+ */
+const computeCentroids = (points, closestCentroids, K) => {
+	let centroids = Array(K)
+		.fill(0)
+		.map(() => Array(3).fill(0));
+	let count = Array(K).fill(0);
+	for (let i = 0; i < points.length; i++) {
+		let centroidIndex = closestCentroids[i];
+		count[centroidIndex]++;
+		for (let j = 0; j < 3; j++) {
+			centroids[centroidIndex][j] += points[i][j];
+		}
+	}
+	for (let i = 0; i < K; i++) {
+		for (let j = 0; j < 3; j++) {
+			centroids[i][j] /= count[i];
+		}
+	}
+	return centroids;
+};
+
+/**
+ * Implements the KMeans clustering algorithm for RGB points, with a fixed number of iterations as the convergence criteria.
+ *
+ */
+const kMeans = (points, K) => {
+	let centroids = Array(K)
+		.fill(0)
+		.map(() => Array(3).fill(0));
+	let closestCentroids = Array(points.length).fill(0);
+
+	for (let i = 0; i < K; i++) {
+		for (let j = 0; j < 3; j++) {
+			centroids[i][j] = points[i][j];
+		}
+	}
+
+	let change = true;
+	while (change) {
+		change = false;
+
+		for (let i = 0; i < points.length; i++) {
+			let closestCentroidIndex = findClosestCentroid(points[i], centroids);
+			if (closestCentroidIndex !== closestCentroids[i]) {
+				closestCentroids[i] = closestCentroidIndex;
+				change = true;
+			}
+		}
+
+		let newCentroids = computeCentroids(points, closestCentroids, K);
+
+		for (let i = 0; i < newCentroids.length; i++) {
+			let count = 0;
+			for (let j = 0; j < 3; j++) {
+				if (isNaN(newCentroids[i][j])) {
+					newCentroids[i][j] = centroids[i][j];
+				} else {
+					count++;
+				}
+			}
+			if (count === 0) {
+				newCentroids[i] = centroids[i];
+			}
+		}
+
+		if (JSON.stringify(newCentroids) !== JSON.stringify(centroids)) {
+			centroids = newCentroids;
+			change = true;
+		}
+	}
+	return centroids;
+};
+/**
  * Transposes a given matrix
  *
  * @param {Array<Array<number>>} matrix - 2D array to be transposed
@@ -25,7 +136,7 @@ const transpose = (matrix) => {
 /**
  * Converts pixel data into arrays for x, y, and z values
  *
- * @param {Array<Array<number>>} pixelArray - Array of pixel data
+ * @param {Array<Array<number>>} pixelArray - Array of arrays of rgb data
  * @returns {Array<Array<number>>} - Array of arrays containing x, y, and z values
  */
 const fillXYZ = (pixelArray) => {
@@ -81,25 +192,49 @@ function downSample(arr, factor) {
  */
 
 const CustomPlot = ({ pixelData }) => {
-	const [xVal, yVal, zVal] = fillXYZ(downSample(pixelData, 15));
+	if (pixelData.length === 0) {
+		console.log("no image detected");
+		return <></>;
+	}
+	const pixelDataShrink = downSample(pixelData, 15);
+	console.log(pixelDataShrink);
+	const clusters = kMeans(pixelDataShrink, 3);
+	console.log(clusters);
+	const [xVal, yVal, zVal] = fillXYZ(pixelDataShrink);
+	const [centroidX, centroidY, centroidZ] = fillXYZ(clusters);
 	const rgb = formatRGB([xVal, yVal, zVal]);
+	const trace1 = {
+		type: "scatter3d",
+		mode: "markers",
+		x: centroidX,
+		y: centroidY,
+		z: centroidZ,
+		marker: {
+			size: 6,
+			color: "black",
+			line: {
+				width: 2,
+				color: "red",
+			},
+		},
+	};
+	const trace2 = {
+		type: "scatter3d",
+		mode: "markers",
+		x: xVal,
+		y: yVal,
+		z: zVal,
+		marker: {
+			size: 3,
+			color: rgb,
+			opacity: 0.1,
+		},
+	};
 
 	return (
 		<>
 			<Plot
-				data={[
-					{
-						type: "scatter3d",
-						mode: "markers",
-						x: xVal,
-						y: yVal,
-						z: zVal,
-						marker: {
-							size: 3,
-							color: rgb,
-						},
-					},
-				]}
+				data={[trace1, trace2]}
 				layout={{
 					autosize: false,
 					width: 400,
