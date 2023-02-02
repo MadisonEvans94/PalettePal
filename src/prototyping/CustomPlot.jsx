f63d26c882e3db8e1d9a87debb3802d7ec7cc4af
Madison Evans <m.corbinevans@gmail.com>
2023-02-02 09:14:39 -0700

    JSDoc comments added

--- src/prototyping/CustomPlot.jsx
+++ src/prototyping/CustomPlot.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Plot from "react-plotly.js";
 
+/**
+ * Transposes a given matrix
+ *
+ * @param {Array<Array<number>>} matrix - 2D array to be transposed
+ * @returns {Array<Array<number>>} - Transposed matrix
+ */
 const transpose = (matrix) => {
 	const transposedMatrix = [];
 	const rows = matrix.length;
@@ -16,6 +22,12 @@ const transpose = (matrix) => {
 	return transposedMatrix;
 };
 
+/**
+ * Converts pixel data into arrays for x, y, and z values
+ *
+ * @param {Array<Array<number>>} pixelArray - Array of pixel data
+ * @returns {Array<Array<number>>} - Array of arrays containing x, y, and z values
+ */
 const fillXYZ = (pixelArray) => {
 	const xVal = [];
 	const yVal = [];
@@ -30,12 +42,26 @@ const fillXYZ = (pixelArray) => {
 	return [xVal, yVal, zVal];
 };
 
+/**
+ * Converts RGB data into CSS-style color strings
+ *
+ * @param {Array<Array<number>>} rgb - Array of RGB values
+ * @returns {Array<string>} - Array of CSS-style color strings
+ */
 const formatRGB = (rgb) => {
 	const transposed = transpose(rgb);
 	return transposed.map(
 		(point) => `rgb(${point[0]}, ${point[1]}, ${point[2]})`
 	);
 };
+
+/**
+ * Down-samples an array by keeping every nth value
+ *
+ * @param {Array<any>} arr - Array to be down-sampled
+ * @param {number} factor - Factor by which to down-sample the array (i.e., keep every nth value)
+ * @returns {Array<any>} - Down-sampled array
+ */
 function downSample(arr, factor) {
 	const newArray = [];
 	for (let i = 0; i < arr.length; i++) {
@@ -46,6 +72,13 @@ function downSample(arr, factor) {
 	return newArray;
 }
 
+/**
+ * Component that displays a 3D scatter plot
+ *
+ * @param {Object} props - Component props
+ * @param {Array<Array<number>>} props.pixelData - Array of pixel data
+ * @returns {React.Component} - 3D scatter plot
+ */
 const CustomPlot = ({ pixelData }) => {
 	const [xVal, yVal, zVal] = fillXYZ(downSample(pixelData, 15));
 	const rgb = formatRGB([xVal, yVal, zVal]);
