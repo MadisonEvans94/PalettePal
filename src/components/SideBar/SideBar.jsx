eea11ae4e7c2ee44af55afaca91f4911077f3a72
Madison Evans <m.corbinevans@gmail.com>
2023-02-02 15:33:29 -0700

    css changes

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import videoUrl from "../../assets/paint.mp4";
 import "./SideBar.css";
 
 /**
@@ -11,18 +11,30 @@ const SideBar = () => {
 	return (
 		<>
 			<div className="sidebar-container">
-				<div className="header">
+				<video
+					style={{
+						position: "absolute",
+						width: "100%",
+						height: "100%",
+						top: 0,
+						left: 0,
+						objectFit: "cover",
+						opacity: 0.5,
+					}}
+					autoPlay
+					loop
+					muted>
+					<source src={videoUrl} type="video/mp4" />
+				</video>
+
+				<div
+					style={{
+						margin: "0 0 2rem 0",
+						position: "relative",
+					}}>
 					<h1>Palette Pal</h1>
-					<p>
-						Welcome to Palette Pal, an interactive web tool designed to help you
-						develop a useful color scheme based on a reference image. Click on
-						the canvas to upload a reference photo, or drag and drop an image to
-						begin
-					</p>
+					<p>Welcome to Palette Pal, an interactive color picker!</p>
 					{/* image input button */}
-				</div>
-				<div className="links">
-					<a href="https://github.com/">github</a>
 				</div>
 			</div>
 		</>
