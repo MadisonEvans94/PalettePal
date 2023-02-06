5bbb500009b4661bd495fc1ff768cdefeacd907f
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    cleanup

--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import "./Palette.css";
 
-/**
- * Palette component that displays a list of colors
- * @param {Object} props - The component properties
- * @param {Array} props.paletteArray - An array of color strings
- */
 const Palette = ({ paletteArray }) => {
-	console.log(
-		"palette component rendering",
-		"centroid RGBs in palette component are ",
-		paletteArray
-	);
 	const colorList = paletteArray.map((color, index) => (
 		<li key={index} className="colorBlock" style={{ margin: "4rem 1rem" }}>
 			<div
 				style={{
 					background: `${color}`,
-
 					borderRadius: "2rem",
 					height: "100%",
 					width: "100%",
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import videoUrl from "../../assets/paint.mp4";
 import "./SideBar.css";
 
-/**
- * @module SideBar
- * @desc A component that renders the sidebar of the Palette Pal app
- * @returns {JSX.Element} The JSX element that renders the SideBar component
- */
 const SideBar = () => {
 	return (
 		<>
