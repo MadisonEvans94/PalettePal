b18e33fe28132eef8cd92c1163267acb2ab0c623
Madison Evans <m.corbinevans@gmail.com>
2023-03-08 11:56:29 -0700

    copied to clip board alert added

--- src/components/ClipboardCopyButton/ClipboardCopyButton.jsx
+++ src/components/ClipboardCopyButton/ClipboardCopyButton.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ClipboardCopyButton({ contentToCopy }) {
+	const [showCopiedMessage, setShowCopiedMessage] = useState(false);
+
 	const handleCopyClick = () => {
 		navigator.clipboard.writeText(contentToCopy);
+		setShowCopiedMessage(true);
+		setTimeout(() => setShowCopiedMessage(false), 3000);
 	};
 
 	return (
-		<button
-			className="p-3 my-3 border rounded border-white"
-			onClick={handleCopyClick}>
-			copy to clipboard
-		</button>
+		<div>
+			<button
+				className="p-3 my-3 border rounded border-white"
+				onClick={handleCopyClick}>
+				copy to clipboard
+			</button>
+			{showCopiedMessage && (
+				<span style={{ marginLeft: "10px" }}>Copied to clipboard!</span>
+			)}
+		</div>
 	);
 }
 
