b26912edcfa7033fdce5b68efc99e2888bd6fa51
Madison Evans <m.corbinevans@gmail.com>
2023-05-15 14:21:17 -0700

    modifying layout

--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,48 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { CentroidContext } from "../../Contexts/CentroidContext";
+import { motion, AnimatePresence } from "framer-motion";
 const Palette = ({ centroid_rgb }) => {
 	const { centroidArray } = useContext(CentroidContext);
+	const [copiedColor, setCopiedColor] = useState(null);
 
-	// const colorList = centroidArray[2].map((color, index) => (
-	// 	<div
-	// 		key={index}
-	// 		className="
-	// 		h-full w-48 border
-	// 		md:mx-2"
-	// 		style={{
-	// 			background: `${color}`,
-	// 		}}
-	// 	/>
-	// ));
+	const testColors = ["#F0F", "#F00", "#0F0", "#000"];
+
+	const copyToClipboard = (color) => {
+		navigator.clipboard.writeText(color);
+		setCopiedColor(color);
+
+		// Remove the notification after 3 seconds
+		setTimeout(() => {
+			setCopiedColor(null);
+		}, 1000);
+	};
+
+	const colorList = testColors.map((color, index) => (
+		<div
+			key={index}
+			onClick={() => copyToClipboard(color)}
+			className="cursor-pointer my-10 relative group h-48 w-48 border md:mx-2 rounded-full flex-none"
+			style={{
+				background: `${color}`,
+			}}>
+			<p className="absolute left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 group-hover:-translate-y-6 transition-all duration-200">
+				{color}
+			</p>
+
+			<AnimatePresence mode="wait">
+				{copiedColor === color && (
+					<motion.p
+						initial={{ opacity: 0 }}
+						animate={{
+							opacity: 1,
+
+							transition: { type: "spring" },
+						}}
+						exit={{ opacity: 0 }}
+						className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white bg-black p-2 rounded text-xs">
+						Color copied to clipboard!
+					</motion.p>
+				)}
+			</AnimatePresence>
+		</div>
+	));
 
 	return (
 		<>
-			<div className="w-screen overflow-x-auto h-48 flex flex-row justify-center">
-				<div
-					className="h-48 w-48 border md:mx-2 rounded-full flex-none"
-					style={{
-						background: "#F0F",
-					}}
-				/>
-				<div
-					className="h-48 w-48 border md:mx-2 rounded-full flex-none"
-					style={{
-						background: "#0FF",
-					}}
-				/>
-				<div
-					className="h-48 w-48 border md:mx-2 rounded-full flex-none"
-					style={{
-						background: "#00F",
-					}}
-				/>
-				<div
-					className="h-48 w-48 border md:mx-2 rounded-full flex-none"
-					style={{
-						background: "#00F",
-					}}
-				/>
+			<div className="border w-screen overflow-x-auto h-fit flex flex-row justify-center">
+				{colorList}
 			</div>
 		</>
 	);
