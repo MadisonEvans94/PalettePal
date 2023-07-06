2465c744c9290ef7bcc925170103c9b1ffac88c7
Madison Evans <m.corbinevans@gmail.com>
2023-07-05 19:58:45 -0700

    styling updates

--- src/components/Dashpane/Dashpane.jsx
+++ src/components/Dashpane/Dashpane.jsx
@@ -28,13 +28,33 @@ const testInfo = [
 const Dashpane = () => {
 	return (
 		<div className="w-full p-10">
-			{testInfo.map((palette) => {
-				return (
+			{testInfo.length === 0 ? (
+				<div className="flex justify-center">
+					<div className="flex flex-col items-center border-2 border-dashed border-gray-400 rounded-lg p-4 w-96 text-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-24 w-24"
+							viewBox="0 0 20 20">
+							<path
+								fillRule="evenodd"
+								fill="#A0AEC0"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-7.536 5.879a1 1 0 001.415 0 3 3 0 014.242 0 1 1 0 001.415-1.415 5 5 0 00-7.072 0 1 1 0 000 1.415z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<p className="text-2xl text-gray-400">No Palettes</p>
+						<p className="text-sm my-4 text-gray-400">
+							Get started by adding a new image from the navbar
+						</p>
+					</div>
+				</div>
+			) : (
+				testInfo.map((palette) => (
 					<div className="my-4">
 						<PaletteCard paletteInfo={palette} />
 					</div>
-				);
-			})}
+				))
+			)}
 		</div>
 	);
 };
