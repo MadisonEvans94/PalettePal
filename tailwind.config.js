d74b4fbc441b6230f190a8d97b5bb480ea2199bf
Madison Evans <m.corbinevans@gmail.com>
2023-07-13 10:09:43 -0700

    configuring tailwind theme updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,55 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#06424e",
+				secondary: "#078c72",
+				success: "#59b447",
+				info: "#3070b8",
+				warning: "#d9822b",
+				danger: "#dc3545",
+			},
+			fontFamily: {
+				sans: ["Open Sans", "sans-serif"],
+				serif: ["Merriweather", "serif"],
+			},
+			spacing: {
+				1: "8px",
+				2: "12px",
+				3: "16px",
+				4: "24px",
+				5: "32px",
+				6: "48px",
+			},
+			borderRadius: {
+				none: "0",
+				sm: "2px",
+				DEFAULT: "4px",
+				md: "6px",
+				lg: "8px",
+				full: "9999px",
+			},
+			boxShadow: {
+				sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+				DEFAULT:
+					"0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+				md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+				lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+				xl: "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+				"2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+				"3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+				inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+				none: "none",
+			},
+			screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+				"2xl": "1536px",
+			},
+		},
 	},
 	plugins: [],
 };
