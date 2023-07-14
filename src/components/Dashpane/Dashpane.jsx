f74896c2936a21cc6cc638084beaf02527658d3d
Madison Evans <m.corbinevans@gmail.com>
2023-07-14 11:40:17 -0700

    functionalizing dash

--- src/components/Dashpane/Dashpane.jsx
+++ src/components/Dashpane/Dashpane.jsx
@@ -3,30 +3,6 @@ import PaletteCard from "../PaletteCard/PaletteCard";
 import { useContext, useEffect } from "react";
 import { AccountContext } from "../Account";
 
-const testInfo = [
-	{
-		name: "Building Photo",
-		date: "11/17/1994",
-		palette: ["#040505", "#475861", "#849da9", "#f7fafc", "#c1d5dd"],
-		imgUrl:
-			"https://images.unsplash.com/photo-1464938050520-ef2270bb8ce8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80",
-	},
-	{
-		name: "Orange Flowers",
-		date: "11/17/1994",
-		palette: ["#000000", "#de9510", "#5b9b9e", "#8c7b3a", "#0a84ae"],
-		imgUrl:
-			"https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
-	},
-	{
-		name: "Building Photo",
-		date: "11/11/1994",
-		palette: ["#1c0e16", "#f96b20", "#cf342e", "#000000", "#7a2c34"],
-		imgUrl:
-			"https://images.unsplash.com/photo-1614878488982-32dbaad50fee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-	},
-];
-
 const Dashpane = () => {
 	const { tokens } = useContext(AccountContext);
 	const [palettes, setPalettes] = useState(null);
@@ -56,9 +32,7 @@ const Dashpane = () => {
 	return (
 		<div className="w-full p-10">
 			<h1 className="text-primary text-[72px] my-8">Madison's Dashboard</h1>
-			{/* <button onClick={getTest}>
-				Click me
-			</button> */}
+
 			<div className="flex flex-row">
 				<button
 					onClick={() => {
@@ -76,7 +50,7 @@ const Dashpane = () => {
 					+
 				</button>
 			</div>
-			{palettes == null ? (
+			{!palettes ? (
 				<div className="flex justify-center">
 					<div className="flex flex-col items-center p-4 text-center border-2 border-dashed rounded-lg border-shade w-96">
 						<svg
@@ -98,18 +72,21 @@ const Dashpane = () => {
 					</div>
 				</div>
 			) : (
-				<>
-					{palettes.palette[kValue].map((color) => {
-						return (
-							<>
+				<div className="flex flex-row">
+					<div className="border">
+						<img src={palettes.imageUrl} alt="thumbnail" />
+					</div>
+					<div className="flex flex-row border bg-secondary w-96 p-2 justify-center">
+						{palettes.palette[kValue].map((color) => {
+							return (
 								<div
-									className="w-6 h-6 rounded-full"
+									className="w-6 h-6 rounded-full mx-1"
 									style={{ backgroundColor: color }}
 								/>
-							</>
-						);
-					})}
-				</>
+							);
+						})}
+					</div>
+				</div>
 			)}
 		</div>
 	);
