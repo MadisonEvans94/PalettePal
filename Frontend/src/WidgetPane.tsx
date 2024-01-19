30899ac063a5e8bc8c7f1ce69ab3b2471b325911
Madison Evans <madison.evans@postfit.co>
2024-01-19 08:59:41 -0700

    structure update for response value for colors

--- Frontend/src/WidgetPane.tsx
+++ Frontend/src/WidgetPane.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import resp from "./test-json/clusters.json";
+console.log(resp.clusters[0]);
+
+// function rgbToHex(rgbArray: number[]) {
+// 	return (
+// 		"#" +
+// 		rgbArray
+// 			.map((val) => {
+// 				const hex = val.toString(16);
+// 				return hex.length === 1 ? "0" + hex : hex;
+// 			})
+// 			.join("")
+// 	);
+// }
 
 const WidgetPane: React.FC = () => {
 	return (
 		<div className="bg-gray-800 text-white h-full flex items-center">
 			<div className="mx-auto border">
 				<div className="flex flex-row gap-1 justify-center">
-					<PaletteColor color="red" />
+					{resp.clusters.map((cluster) => (
+						<PaletteColor color={cluster} />
+					))}
 				</div>
 				<div>counter goes here</div>
 				<button>copy palette</button>
