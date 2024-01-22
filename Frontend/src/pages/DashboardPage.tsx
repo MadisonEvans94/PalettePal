83057ccb5a22f6246f81b087b0b7f780896125be
Madison Evans <madison.evans@postfit.co>
2024-01-22 10:53:09 -0700

    test files in place

--- Frontend/src/pages/DashboardPage.tsx
+++ Frontend/src/pages/DashboardPage.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import PaletteCard from "../components/PaletteCard";
-import imageInstance from "../test-json/clusters.json";
+import imageInstances from "../test-json/clusters.json";
 
 const DashboardPage: React.FC = () => {
 	return (
 		<div className="min-h-screen bg-gray-100 p-8">
 			<div className="max-w-4xl mx-auto">
 				<div className="space-y-4">
-					<PaletteCard
-						clusterData={imageInstance.clusterData}
-						imageUrl={imageInstance.imageUrl}
-					/>
-					<PaletteCard
-						clusterData={imageInstance.clusterData}
-						imageUrl={imageInstance.imageUrl}
-					/>
-					<PaletteCard
-						clusterData={imageInstance.clusterData}
-						imageUrl={imageInstance.imageUrl}
-					/>
+					{imageInstances.map((imageInstance, id) => {
+						return (
+							<PaletteCard
+								// TODO: Update to use db id in future
+								key={id}
+								clusterData={imageInstance.clusterData}
+								imageUrl={imageInstance.imageUrl}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</div>
