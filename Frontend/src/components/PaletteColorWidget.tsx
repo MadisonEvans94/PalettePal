57f89eac604fc7f923d50b76b14438c6da97ff29
Madison Evans <madison.evans@postfit.co>
2024-03-04 18:53:21 -0700

    copy functionality added to PaletteColor components

--- Frontend/src/components/PaletteColorWidget.tsx
+++ Frontend/src/components/PaletteColorWidget.tsx
@@ -11,11 +11,30 @@ export interface PaletteColorWidgetProps {
 }
 
 const PaletteColor: React.FC<PaletteColorProps> = ({ color }) => {
+	const [isHovered, setIsHovered] = React.useState(false);
+
+	async function copyToClipboard() {
+		try {
+			await navigator.clipboard.writeText(color);
+		} catch (e) {
+			console.error("Copy to clipboard failed", e);
+		}
+	}
+
 	return (
 		<div
 			style={{ backgroundColor: color }}
-			className="w-16 h-16 rounded-sm"
-		/>
+			className="w-16 h-16 rounded-sm cursor-pointer relative"
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+			onClick={copyToClipboard}
+		>
+			{isHovered && (
+				<div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 p-1 bg-black text-white text-xs rounded">
+					{color}
+				</div>
+			)}
+		</div>
 	);
 };
 
