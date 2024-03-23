3a6d7626d77df5d52b378acdeb4a30d31ebbf50f
Madison Evans <madison.evans@postfit.co>
2024-03-23 13:32:57 -0700

    refactoring image pane

--- Frontend/src/components/ImagePane/ImagePane.tsx
+++ Frontend/src/components/ImagePane/ImagePane.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { ImagePaneProps } from "../../types";
-
+import { IoImage } from "react-icons/io5";
 const ImagePane: React.FC<ImagePaneProps> = ({ uploadedImage }) => {
 	return (
-		<div className="text-white h-full flex items-center p-12">
-			<div className="overflow-hidden relative w-full h-full ">
-				{uploadedImage && (
-					<img
-						src={uploadedImage}
-						alt="Uploaded"
-						className="absolute w-full h-full object-contain"
-					/>
-				)}
-			</div>
-		</div>
+		<>
+			{uploadedImage ? (
+				<div className="text-white h-full flex items-center p-12">
+					<div className="overflow-hidden relative w-full h-full ">
+						{uploadedImage && (
+							<img
+								src={uploadedImage}
+								alt="Uploaded"
+								className="absolute w-full h-full object-contain"
+							/>
+						)}
+					</div>
+				</div>
+			) : (
+				<div className=" text-dark border h-full w-full flex flex-col items-center justify-center">
+					<IoImage size="8em" />
+				</div>
+			)}
+		</>
 	);
 };
 
